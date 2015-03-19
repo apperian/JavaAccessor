d1c13aff792ea13c330fc581e35eaac85b1bccb1
Kevin LaFlamme <klaflamme@apperian.com>
2015-03-19 14:18:56 -0400

    Update tests to separate assertions in distinct test cases

--- test/com/apperian/javautil/AccessorTest.java
+++ test/com/apperian/javautil/AccessorTest.java
@@ -12,37 +12,114 @@ import com.apperian.javautil.UnsupportedTypeException;
 public class AccessorTest {
     
     @Test
-    public void invokeMethod() throws NoSuchMethodException, UnsupportedTypeException {
+    public void invokeBooleanMethod() throws NoSuchMethodException, UnsupportedTypeException {
         Target t = new Target();
         assertEquals(t.returnBoolean(),Accessor.invokeMethod(t,"returnBoolean",new Object[0]));
+    }
+    
+    @Test
+    public void invokeByteMethod() throws NoSuchMethodException, UnsupportedTypeException {
+        Target t = new Target();
         assertEquals(t.returnByte(),Accessor.invokeMethod(t,"returnByte",new Object[0]));
+    }
+    
+    @Test
+    public void invokeCharMethod() throws NoSuchMethodException, UnsupportedTypeException {
+        Target t = new Target();
         assertEquals(t.returnChar(),Accessor.invokeMethod(t,"returnChar",new Object[0]));
+    }
+    @Test
+    public void invokeDoubleMethod() throws NoSuchMethodException, UnsupportedTypeException {
+        Target t = new Target();
         assertEquals(t.returnDouble(),Accessor.invokeMethod(t,"returnDouble",new Object[0]));
+    }
+    
+    @Test
+    public void invokeFloatMethod() throws NoSuchMethodException, UnsupportedTypeException {
+        Target t = new Target();
         assertEquals(t.returnFloat(),Accessor.invokeMethod(t,"returnFloat",new Object[0]));
+    }
+    
+    @Test
+    public void invokeIntMethod() throws NoSuchMethodException, UnsupportedTypeException {
+        Target t = new Target();
         assertEquals(t.returnInt(),Accessor.invokeMethod(t,"returnInt",new Object[0]));
+    }
+    
+    @Test
+    public void invokeLongMethod() throws NoSuchMethodException, UnsupportedTypeException {
+        Target t = new Target();
         assertEquals(t.returnLong(),Accessor.invokeMethod(t,"returnLong",new Object[0]));
+    }
+    
+    @Test
+    public void invokeObjectMethod() throws NoSuchMethodException, UnsupportedTypeException {
+        Target t = new Target();
         assertEquals(t.returnObject(),Accessor.invokeMethod(t,"returnObject",new Object[0]));
+    }
+    
+    @Test
+    public void invokeShortMethod() throws NoSuchMethodException, UnsupportedTypeException {
+        Target t = new Target();
         assertEquals(t.returnShort(),Accessor.invokeMethod(t,"returnShort",new Object[0]));
-        
+    }
+    
+    @Test
+    public void invokeVoidMethod() throws NoSuchMethodException, UnsupportedTypeException {
+        Target t = new Target();
         Accessor.invokeMethod(t,"returnVoid",new Object[0]);
         assertEquals(t.instanceInt,t.instanceIntExpected);
     }
     
     @Test
-    public void invokeStaticMethod() throws NoSuchMethodException, UnsupportedTypeException {
+    public void invokeStaticBooleanMethod() throws NoSuchMethodException, UnsupportedTypeException {
         assertEquals(Target.returnStaticBoolean(),Accessor.invokeMethod(Target.class,"returnStaticBoolean",new Object[0]));
+    }
+    
+    @Test
+    public void invokeStaticByteMethod() throws NoSuchMethodException, UnsupportedTypeException {
         assertEquals(Target.returnStaticByte(),Accessor.invokeMethod(Target.class,"returnStaticByte",new Object[0]));
+    }
+    
+    @Test
+    public void invokeStaticCharMethod() throws NoSuchMethodException, UnsupportedTypeException {
         assertEquals(Target.returnStaticChar(),Accessor.invokeMethod(Target.class,"returnStaticChar",new Object[0]));
+    }
+    
+    @Test
+    public void invokeStaticDoubleMethod() throws NoSuchMethodException, UnsupportedTypeException {
         assertEquals(Target.returnStaticDouble(),Accessor.invokeMethod(Target.class,"returnStaticDouble",new Object[0]));
+    }
+    
+    @Test
+    public void invokeStaticFloatMethod() throws NoSuchMethodException, UnsupportedTypeException {
         assertEquals(Target.returnStaticFloat(),Accessor.invokeMethod(Target.class,"returnStaticFloat",new Object[0]));
+    }
+    
+    @Test
+    public void invokeStaticIntMethod() throws NoSuchMethodException, UnsupportedTypeException {
         assertEquals(Target.returnStaticInt(),Accessor.invokeMethod(Target.class,"returnStaticInt",new Object[0]));
+    }
+    
+    @Test
+    public void invokeStaticLongMethod() throws NoSuchMethodException, UnsupportedTypeException {
         assertEquals(Target.returnStaticLong(),Accessor.invokeMethod(Target.class,"returnStaticLong",new Object[0]));
+    }
+    
+    @Test
+    public void invokeStaticObjectMethod() throws NoSuchMethodException, UnsupportedTypeException {
         assertEquals(Target.returnStaticObject(),Accessor.invokeMethod(Target.class,"returnStaticObject",new Object[0]));
+    }
+    
+    @Test
+    public void invokeStaticShortMethod() throws NoSuchMethodException, UnsupportedTypeException {
         assertEquals(Target.returnStaticShort(),Accessor.invokeMethod(Target.class,"returnStaticShort",new Object[0]));
-        
-        Target.returnStaticVoid();
+    }
+    
+    @Test
+    public void invokeStaticVoidMethod() throws NoSuchMethodException, UnsupportedTypeException {
         Accessor.invokeMethod(Target.class,"returnStaticVoid",new Object[0]);
-        assertEquals(Target.staticInt,Target.staticIntExpected);
+        assertEquals(Target.staticIntExpected,Target.staticInt);
     }
 
     
--- test/com/apperian/javautil/MethodsTest.java
+++ test/com/apperian/javautil/MethodsTest.java
@@ -3,7 +3,6 @@ package com.apperian.javautil;
 import static org.junit.Assert.*;
 
 import java.lang.reflect.Method;
-import java.util.HashMap;
 
 import org.junit.Test;
 
@@ -13,91 +12,282 @@ import com.apperian.javautil.Primitives.NativeType;
 public class MethodsTest {
     
     @Test
-    public void testReturnSig() throws NoSuchMethodException, SecurityException {
+    public void testBooleanReturnSig() throws NoSuchMethodException, SecurityException {
         Method method; 
         method = Target.class.getMethod("returnBoolean");
         assertEquals("()Z",Methods.getSignature(method));
-        
+    }
+    
+    @Test
+    public void testByteReturnSig() throws NoSuchMethodException, SecurityException {
+        Method method; 
         method = Target.class.getMethod("returnByte");
         assertEquals("()B",Methods.getSignature(method));
-        
+    }
+    
+    @Test
+    public void testCharReturnSig() throws NoSuchMethodException, SecurityException {
+        Method method; 
         method = Target.class.getMethod("returnChar");
         assertEquals("()C",Methods.getSignature(method));
-        
+    }
+    
+    @Test
+    public void testDoubleReturnSig() throws NoSuchMethodException, SecurityException {
+        Method method; 
         method = Target.class.getMethod("returnDouble");
         assertEquals("()D",Methods.getSignature(method));
-        
+    }
+    
+    @Test
+    public void testFloatReturnSig() throws NoSuchMethodException, SecurityException {
+        Method method; 
         method = Target.class.getMethod("returnFloat");
         assertEquals("()F",Methods.getSignature(method));
-        
+    }
+    
+    @Test
+    public void testIntReturnSig() throws NoSuchMethodException, SecurityException {
+        Method method; 
         method = Target.class.getMethod("returnInt");
         assertEquals("()I",Methods.getSignature(method));
-        
+    }
+    
+    @Test
+    public void testLongReturnSig() throws NoSuchMethodException, SecurityException {
+        Method method; 
         method = Target.class.getMethod("returnLong");
         assertEquals("()J",Methods.getSignature(method));
-        
+    }
+    
+    @Test
+    public void testObjectReturnSig() throws NoSuchMethodException, SecurityException {
+        Method method; 
         method = Target.class.getMethod("returnObject");
         assertEquals("()Lcom/apperian/javautil/Target;",Methods.getSignature(method));
-        
+    }
+    
+    @Test
+    public void testPrimitiveArrayReturnSig() throws NoSuchMethodException, SecurityException {
+        Method method; 
         method = Target.class.getMethod("returnPrimitiveArray");
         assertEquals("()[I",Methods.getSignature(method));
-        
+    }
+    
+    @Test
+    public void testObjectArrayReturnSig() throws NoSuchMethodException, SecurityException {
+        Method method;     
         method = Target.class.getMethod("returnObjectArray");
         assertEquals("()[Lcom/apperian/javautil/Target;",Methods.getSignature(method));
-        
+    }
+    
+    @Test
+    public void testShortReturnSig() throws NoSuchMethodException, SecurityException {
+        Method method; 
         method = Target.class.getMethod("returnShort");
         assertEquals("()S",Methods.getSignature(method));
-        
+    }
+    
+    @Test
+    public void testVoidReturnSig() throws NoSuchMethodException, SecurityException {
+        Method method; 
         method = Target.class.getMethod("returnVoid");
         assertEquals("()V",Methods.getSignature(method));
     }
     
     @Test
-    public void testArgumentSig() throws NoSuchMethodException, SecurityException {
+    public void testBooleanArgumentSig() throws NoSuchMethodException, SecurityException {
         Method method; 
         method = Target.class.getMethod("booleanVoid",boolean.class);
         assertEquals("(Z)V",Methods.getSignature(method));
-        
+    }
+    
+    @Test
+    public void testByteArgumentSig() throws NoSuchMethodException, SecurityException {
+        Method method; 
         method = Target.class.getMethod("byteVoid",byte.class);
         assertEquals("(B)V",Methods.getSignature(method));
-        
+    }
+    
+    @Test
+    public void testCharArgumentSig() throws NoSuchMethodException, SecurityException {
+        Method method; 
         method = Target.class.getMethod("charVoid",char.class);
         assertEquals("(C)V",Methods.getSignature(method));
-        
+    }
+    
+    @Test
+    public void testDoubleArgumentSig() throws NoSuchMethodException, SecurityException {
+        Method method; 
         method = Target.class.getMethod("doubleVoid",double.class);
         assertEquals("(D)V",Methods.getSignature(method));
-        
+    }
+    
+    @Test
+    public void testFloatArgumentSig() throws NoSuchMethodException, SecurityException {
+        Method method; 
         method = Target.class.getMethod("floatVoid",float.class);
         assertEquals("(F)V",Methods.getSignature(method));
-        
+    }
+    
+    @Test
+    public void testIntArgumentSig() throws NoSuchMethodException, SecurityException {
+        Method method; 
         method = Target.class.getMethod("intVoid",int.class);
         assertEquals("(I)V",Methods.getSignature(method));
-        
+    }
+    
+    @Test
+    public void testLongArgumentSig() throws NoSuchMethodException, SecurityException {
+        Method method; 
         method = Target.class.getMethod("longVoid",long.class);
         assertEquals("(J)V",Methods.getSignature(method));
-        
+    }
+    
+    @Test
+    public void testObjectArgumentSig() throws NoSuchMethodException, SecurityException {
+        Method method; 
         method = Target.class.getMethod("objectVoid",Target.class);
         assertEquals("(Lcom/apperian/javautil/Target;)V",Methods.getSignature(method));
-        
+    }
+    
+    @Test
+    public void testPrimitiveArrayArgumentSig() throws NoSuchMethodException, SecurityException {
+        Method method; 
         method = Target.class.getMethod("primitiveArrayVoid",int[].class);
         assertEquals("([I)V",Methods.getSignature(method));
-        
+    }
+    
+    @Test
+    public void testObjectArrayArgumentSig() throws NoSuchMethodException, SecurityException {
+        Method method; 
         method = Target.class.getMethod("objectArrayVoid",Target[].class);
         assertEquals("([Lcom/apperian/javautil/Target;)V",Methods.getSignature(method));
-        
+    }
+    
+    @Test
+    public void testShortArgumentSig() throws NoSuchMethodException, SecurityException {
+        Method method; 
         method = Target.class.getMethod("shortVoid",short.class);
         assertEquals("(S)V",Methods.getSignature(method));
-        
+    }
+    
+    @Test
+    public void testVoidArgumentSig() throws NoSuchMethodException, SecurityException {
+        Method method; 
         method = Target.class.getMethod("voidVoid");
         assertEquals("()V",Methods.getSignature(method));
-        
+    }
+    
+    @Test
+    public void testAllArgumentSig() throws NoSuchMethodException, SecurityException {
+        Method method; 
         method = Target.class.getMethod("allArgsVoid", boolean.class, byte.class, char.class, double.class, float.class, int.class,
                 long.class, Target.class, short.class, int[].class, Target[].class);
         assertEquals("(ZBCDFIJLcom/apperian/javautil/Target;S[I[Lcom/apperian/javautil/Target;)V",Methods.getSignature(method));
     }
     
     @Test
-    public void testArgumentTypes() throws NoSuchMethodException, SecurityException {
+    public void testBooleanArgumentType() throws NoSuchMethodException, SecurityException {
+        Method method;
+        int[] argTypes;
+        method = Target.class.getMethod("booleanVoid",boolean.class);
+        argTypes = Methods.getArgTypes(method);
+        assertEquals(NativeType.BOOLEAN,argTypes[0]);
+    }
+    
+    @Test
+    public void testByteArgumentType() throws NoSuchMethodException, SecurityException {
+        Method method;
+        int[] argTypes;
+        method = Target.class.getMethod("byteVoid",byte.class);
+        argTypes = Methods.getArgTypes(method);
+        assertEquals(NativeType.BYTE,argTypes[0]);
+    }
+    
+    @Test
+    public void testCharArgumentType() throws NoSuchMethodException, SecurityException {
+        Method method;
+        int[] argTypes;
+        method = Target.class.getMethod("charVoid",char.class);
+        argTypes = Methods.getArgTypes(method);
+        assertEquals(NativeType.CHAR,argTypes[0]);
+    }
+    
+    @Test
+    public void testDoubleArgumentType() throws NoSuchMethodException, SecurityException {
+        Method method;
+        int[] argTypes;
+        method = Target.class.getMethod("doubleVoid",double.class);
+        argTypes = Methods.getArgTypes(method);
+        assertEquals(NativeType.DOUBLE,argTypes[0]);
+    }
+    
+    @Test
+    public void testFloatArgumentType() throws NoSuchMethodException, SecurityException {
+        Method method;
+        int[] argTypes;
+        method = Target.class.getMethod("floatVoid",float.class);
+        argTypes = Methods.getArgTypes(method);
+        assertEquals(NativeType.FLOAT,argTypes[0]);
+    }
+    
+    @Test
+    public void testIntArgumentType() throws NoSuchMethodException, SecurityException {
+        Method method;
+        int[] argTypes;
+        method = Target.class.getMethod("intVoid",int.class);
+        argTypes = Methods.getArgTypes(method);
+        assertEquals(NativeType.INT,argTypes[0]);
+    }
+    
+    @Test
+    public void testLongArgumentType() throws NoSuchMethodException, SecurityException {
+        Method method;
+        int[] argTypes;
+        method = Target.class.getMethod("longVoid",long.class);
+        argTypes = Methods.getArgTypes(method);
+        assertEquals(NativeType.LONG,argTypes[0]);
+    }
+    
+    @Test
+    public void testObjectArgumentType() throws NoSuchMethodException, SecurityException {
+        Method method;
+        int[] argTypes;
+        method = Target.class.getMethod("objectVoid",Target.class);
+        argTypes = Methods.getArgTypes(method);
+        assertEquals(NativeType.OBJECT,argTypes[0]);
+    }
+    
+    @Test
+    public void testPrimitiveArrayArgumentType() throws NoSuchMethodException, SecurityException {
+        Method method;
+        int[] argTypes;
+        method = Target.class.getMethod("primitiveArrayVoid",int[].class);
+        argTypes = Methods.getArgTypes(method);
+        assertEquals(NativeType.OBJECT,argTypes[0]);
+    }
+    
+    @Test
+    public void testObjectArrayArgumentType() throws NoSuchMethodException, SecurityException {
+        Method method;
+        int[] argTypes;
+        method = Target.class.getMethod("objectArrayVoid",Target[].class);
+        argTypes = Methods.getArgTypes(method);
+        assertEquals(NativeType.OBJECT,argTypes[0]);
+    }
+    
+    @Test
+    public void testShortArgumentType() throws NoSuchMethodException, SecurityException {
+        Method method;
+        int[] argTypes;
+        method = Target.class.getMethod("shortVoid",short.class);
+        argTypes = Methods.getArgTypes(method);
+        assertEquals(NativeType.SHORT,argTypes[0]);
+    }
+    
+    @Test
+    public void testAllArgumentsType() throws NoSuchMethodException, SecurityException {
         Method method;
         method = Target.class.getMethod("allArgsVoid", boolean.class, byte.class, char.class, double.class, float.class, int.class,
                 long.class, Target.class, short.class, int[].class, Target[].class);
